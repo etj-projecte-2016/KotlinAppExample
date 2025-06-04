7648de32549cd1ccad23d3dde98d16efd6f88167
Eric <eric.torres.jara@gmail.com>
2025-06-04 20:23:08 +0200

    initial commit

---
+++ app/src/main/java/com/example/appexample1/BlankFragment.kt
@@ -0,0 +1,40 @@
+package com.example.appexample1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.appexample1.databinding.FragmentBlankBinding
+
+class BlankFragment : Fragment() {
+
+    private var _binding: FragmentBlankBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        _binding = FragmentBlankBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonBlank.setOnClickListener {
+
+            findNavController().navigate(R.id.action_blankFragment_to_FirstFragment)
+
+        }
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+}
